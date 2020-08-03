aba63d71eaeb82cd75dc385ce7ca224bf270cace
Dejazmach Getachew <jilogical@gmail.com>
2020-08-03 23:12:55 +0300

    Fixed issues from review

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# rubocop: disable Metrics/PerceivedComplexity
 
 puts '  ________  ______   ______       ________  ______    ______       ________  ______   ________ '
 puts ' /        |/      | /      \     /        |/      \  /      \     /        |/      \ /        |'
@@ -11,29 +12,89 @@ puts '    $$ |   / $$   |$$    $$/        $$ |  $$ |  $$ |$$    $$/        $$ | 
 puts '    $$/    $$$$$$/  $$$$$$/         $$/   $$/   $$/  $$$$$$/         $$/    $$$$$$/  $$$$$$$$/ '
 
 puts
+puts '==================================NOTE========================================'
+puts 'PLEASE TRY REPEATEDLY TO SEE THE WHOLE PICTURE AS THE GAME IS NOT COMPLETE YET'
+puts '=============================================================================='
+puts
 puts 'Player 1: Enter Your Name'
-player_1_name = gets.chomp
+$player_1_name = gets.chomp
 puts 'Player 2: Enter Your Name'
-player_2_name = gets.chomp
+$player_2_name = gets.chomp
+$board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 def display_board(*args)
-  # This board variable doesn't belong here, it belongs to a Game class
-  board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  board[1][1] = 'X' unless args[0].nil?
+  $board[1][1] = 'X' unless args[0].nil?
   puts ' _________________________ '
   puts '|__________BOARD__________|'
   puts '|                         |'
-  puts "|      #{board[0][0]}     #{board[0][1]}     #{board[0][2]}      |"
+  puts "|      #{$board[0][0]}     #{$board[0][1]}     #{$board[0][2]}      |"
   puts '|                         |'
-  puts "|      #{board[1][0]}     #{board[1][1]}     #{board[1][2]}      |"
+  puts "|      #{$board[1][0]}     #{$board[1][1]}     #{$board[1][2]}      |"
   puts '|                         |'
-  puts "|      #{board[2][0]}     #{board[2][1]}     #{board[2][2]}      |"
+  puts "|      #{$board[2][0]}     #{$board[2][1]}     #{$board[2][2]}      |"
   puts '|_________________________|'
 end
 
-display_board
-puts "#{player_1_name}'s turn! Pick a number pick 5 please :D"
-entry = gets.chomp
-display_board(entry)
-puts "#{player_2_name}'s turn!"
-gets.chomp
+def reset_board
+  $board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+end
+
+def start
+  continue_game = true
+  while continue_game
+    reset_board
+    play_game
+    puts 'Do you want to play again? Enter Y to continue or any other character to quit.'
+    continue_game_char = gets.chomp
+    continue_game = %w[y Y].include?(continue_game_char)
+  end
+end
+
+def play_game
+  while !game_won || !game_drawn
+    display_board
+    puts
+    puts "#{$player_1_name}'s turn!"
+    entry = find_number
+
+    display_board(entry)
+    puts
+    puts "#{$player_2_name}'s turn!"
+    entry = find_number
+  end
+  if game_won
+    puts "Congratulations #{$player_1_name}. You won the game."
+  elsif game_drawn
+    puts 'Game Drawn!'
+  end
+end
+
+def find_number
+  not_valid_entry = true
+  while not_valid_entry
+    entry = gets.chomp.to_i
+    if entry < 1 || entry > 9
+      puts 'Invalid Move : The number should be between 1 and 9.'
+    else
+      index = (entry % 3).zero? ? ((entry / 3) - 1).to_i : (entry / 3).to_i
+      if $board[index].none? { |val| val == entry.to_i }
+        puts 'Invalid Move : The number is already taken, try another' # Try this with 5 after first move
+      else
+        not_valid_entry = false
+      end
+    end
+  end
+  entry
+end
+
+def game_won
+  rand(1..10) > 6
+end
+
+def game_drawn
+  rand(1..10) > 6
+end
+
+start
+
+# rubocop: enable Metrics/PerceivedComplexity
