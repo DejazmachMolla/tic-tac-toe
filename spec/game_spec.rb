eb266bbd09bbb6253bab713a0f6c1485ec8409c1
Dejazmach Getachew <jilogical@gmail.com>
2020-08-10 15:52:44 +0300

    Extracted out initialization to avoid code repetition

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,13 +3,13 @@ require_relative '../lib/player.rb'
 
 #spec/game_spec.rb
 describe Game do
-  describe "#won" do
-    let (:players) { ['d', 'j'] }
-    let (:player1) { Player.new(players[0], 'X') }
-    let (:player2) { Player.new(players[1], 'O') }
-    let (:game1) { Game.new([['X', 'X', 'X'], [4, 5, 6], [7, 8, 9]], player1, player2) }
-    let (:game2) { Game.new([['X', 'X', 'O'], [4, 5, 6], [7, 8, 9]], player1, player2) }
+  let (:players) { ['d', 'j'] }
+  let (:player1) { Player.new(players[0], 'X') }
+  let (:player2) { Player.new(players[1], 'O') }
+  let (:game1) { Game.new([['X', 'X', 'X'], [4, 5, 6], [7, 8, 9]], player1, player2) }
+  let (:game2) { Game.new([['X', 'X', 'O'], [4, 5, 6], [7, 8, 9]], player1, player2) }
 
+  describe "#won" do
     it "returns true because game is won" do
       expect(game1.won).to eql(true)
     end
